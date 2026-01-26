90023254fcbc5761fbdf2b3385fa808ccb8bddcb
root <root@srv1297188.hstgr.cloud>
2026-01-26 02:41:04 +0000

    123

--- src/hooks/useSystemURLCon.js
+++ src/hooks/useSystemURLCon.js
@@ -1,6 +1,6 @@
 const useSystemURLCon = () => {
-    const url = "http://127.0.0.1:8000/api";
-    const urlWithoutToken = "http://127.0.0.1:8000";
+    const url = "http://72.62.116.26/api";
+    const urlWithoutToken = "http://72.62.116.26";
 
     return { url, urlWithoutToken };
 }
