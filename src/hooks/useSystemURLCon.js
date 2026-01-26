c00faa22f4481b61bc74b414fb0ef7ca97a7029c
John The Bryan <johnbryanjavellana@gmail.com>
2026-01-26 10:52:59 +0800

    123

--- src/hooks/useSystemURLCon.js
+++ src/hooks/useSystemURLCon.js
@@ -1,6 +1,9 @@
 const useSystemURLCon = () => {
-    const url = "http://72.62.116.26/api";
-    const urlWithoutToken = "http://72.62.116.26";
+    // const url = "http://72.62.116.26/api";
+    // const urlWithoutToken = "http://72.62.116.26";
+
+    const url = "http://127.0.0.1:8000/api";
+    const urlWithoutToken = "http://127.0.0.1:8000";
 
     return { url, urlWithoutToken };
 }
