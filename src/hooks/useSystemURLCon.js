4e6c4f6901b12bce49d053e675c7ee42dcf9677c
John The Bryan <johnbryanjavellana@gmail.com>
2026-01-26 11:33:47 +0800

    123

--- src/hooks/useSystemURLCon.js
+++ src/hooks/useSystemURLCon.js
@@ -1,7 +1,7 @@
 const useSystemURLCon = () => {
     // Use the domain name and https
-    const url = "https://accatjps.online/api";
-    const urlWithoutToken = "https://accatjps.online";
+    const url = "https://127.0.0.1:8000/api";
+    const urlWithoutToken = "https://127.0.0.1:8000";
 
     return { url, urlWithoutToken };
 }
